2e3f44e20e21593bc768e538431efe6a2f8857d2
Christopher Bull <caffeinatedandroid@gmail.com>
2020-02-10 10:07:24 +0000

    Test: NetTcpTest - Init ServerSocket BeforeClass

--- app/src/test/java/caffeinatedandroid/rpibotcontrol/NetTcpTest.kt
+++ app/src/test/java/caffeinatedandroid/rpibotcontrol/NetTcpTest.kt
@@ -3,11 +3,37 @@ package caffeinatedandroid.rpibotcontrol
 import caffeinatedandroid.rpibotcontrol.net.http.TCP
 import caffeinatedandroid.rpibotcontrol.net.http.TCP.Companion.defaultTcpPort
 import com.google.common.truth.Truth.assertThat
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 import java.net.ServerSocket
 import kotlin.concurrent.thread
 
 class NetTcpTest {
+
+    companion object {
+        lateinit var server: ServerSocket
+
+        @BeforeClass
+        @JvmStatic
+        fun setupServerSocket() {
+            // Simple test TCP server to test connections
+            server = ServerSocket(defaultTcpPort)
+            server.soTimeout = 5000
+            thread(start = true) {
+                server.use { server ->
+                    server.accept()
+                }
+            }
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun cleanupServerSocket() {
+            server.close()
+        }
+    }
+
     @Test
     fun tcpReportsNotConnectedYet() {
         val tcp = TCP("localhost")
@@ -17,15 +43,6 @@ class NetTcpTest {
 
     @Test
     fun tcpSocketConnectsToServer() {
-        // Simple test TCP server to test connections
-        thread(start = true) {
-            val server = ServerSocket(defaultTcpPort)
-            server.soTimeout = 1000
-            server.use { server ->
-                server.accept()
-            }
-        }
-
         // Initialise TCP then connect to test server
         val tcp = TCP("localhost")
         var isConnected = false
