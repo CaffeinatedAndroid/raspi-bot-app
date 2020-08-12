11c89caed61322248b3c8e9814fd89b1b9b79de1
Christopher Bull <caffeinatedandroid@gmail.com>
2020-08-12 12:59:40 +0100

    Docs: Added KDoc to TCP and UDP

--- app/src/main/java/caffeinatedandroid/rpibotcontrol/net/http/TCP.kt
+++ app/src/main/java/caffeinatedandroid/rpibotcontrol/net/http/TCP.kt
@@ -7,9 +7,15 @@ import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.Socket
 
+/**
+ * A TCP network connection.
+ */
 class TCP(private val host: String, private val port: Int = defaultPort) : IConnection {
 
     companion object {
+        /**
+         * The default TCP port number.
+         */
         const val defaultPort = 9020
     }
 
@@ -17,21 +23,33 @@ class TCP(private val host: String, private val port: Int = defaultPort) : IConn
     private lateinit var out: PrintWriter
     private lateinit var `in`: BufferedReader
 
+    /**
+     * Connects to the remote socket
+     */
     override fun connect() {
         clientSocket = Socket(host, port)
         out = PrintWriter(clientSocket!!.getOutputStream(), true)
         `in` = BufferedReader(InputStreamReader(clientSocket!!.getInputStream()))
     }
 
+    /**
+     * Determines if this connection is connected.
+     */
     override fun isConnected(): Boolean {
         return clientSocket?.isConnected ?: false
     }
 
+    /**
+     * Sends a message to the connected device.
+     */
     override fun send(msgType: MessageType, msg: String): String? {
         out.println(msg)
         return `in`.readLine()
     }
 
+    /**
+     * Closes the connection to the device.
+     */
     override fun close() {
         if (this::`in`.isInitialized) {
             `in`.close()
--- app/src/main/java/caffeinatedandroid/rpibotcontrol/net/http/UDP.kt
+++ app/src/main/java/caffeinatedandroid/rpibotcontrol/net/http/UDP.kt
@@ -9,19 +9,35 @@ import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 
-
+/**
+ * A UDP network connection.
+ */
 class UDP(host: String, private val port: Int = defaultPort) : IConnection {
 
     companion object {
 
+        /**
+         * The default UDP port number.
+         */
         const val defaultPort = 9021
         private const val defaultBroadcastPort = 9022
         private const val defaultBroadcastTcpResponsePort = 9023
 
-        // Copies of local and server ports - they can be made different (e.g., for 'localhost' testing)
+        /**
+         * Copy of the default port. Used for 'localhost' testing, when the local and remote
+         * port numbers would clash.
+         */
         var serverPort = defaultPort
+
+        /**
+         * Copy of the broadcast port. Used for 'localhost' testing, when the local and remote
+         * port numbers would clash.
+         */
         var serverBroadcastPort = defaultBroadcastPort
 
+        /**
+         * Discover available devices that can connect via UDP
+         */
         fun discoverConnections(
             context: Context,
             broadcast_port: Int = defaultBroadcastPort
@@ -41,8 +57,11 @@ class UDP(host: String, private val port: Int = defaultPort) : IConnection {
             // TODO Listen for responses
         }
 
+        /**
+         * Gets the broadcast [InetAddress] from the [WifiManager]
+         */
         @Throws(IOException::class)
-        fun getBroadcastAddress(context: Context): InetAddress? {
+        private fun getBroadcastAddress(context: Context): InetAddress? {
             val wifi: WifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
             val dhcp = wifi.dhcpInfo
             val broadcast = dhcp.ipAddress and dhcp.netmask or dhcp.netmask.inv()
@@ -55,18 +74,25 @@ class UDP(host: String, private val port: Int = defaultPort) : IConnection {
     private var udpSocket: DatagramSocket? = null
     private val serverAddress: InetAddress = InetAddress.getByName(host)
 
-    // Copies of local and server ports - they can be made different (e.g., for 'localhost' testing)
-//    var serverPort = port
-//    var serverBroadcastPort = broadcast_port
-
+    /**
+     * Binds the [DatagramSocket] to a port.
+     */
     override fun connect() {
         udpSocket = DatagramSocket(port)
     }
 
+    /**
+     * Determines if the [DatagramSocket] is bound.
+     * Unlike other [IConnection], UDP does not connect (like TCP), but ports
+     * are reserved and bound to a [DatagramSocket].
+     */
     override fun isConnected(): Boolean {
         return udpSocket?.isBound ?: false
     }
 
+    /**
+     * Sends a message to the connected device.
+     */
     override fun send(msgType: MessageType, msg: String): String? {
         if (udpSocket == null) {
             throw IOException("DatagramSocket not bound. Connect first to bind to port.")
@@ -77,38 +103,11 @@ class UDP(host: String, private val port: Int = defaultPort) : IConnection {
         return null
     }
 
+    /**
+     * Closes the connection to the device.
+     * Unbinds the port from the [DatagramSocket]
+     */
     override fun close() {
         udpSocket?.close()
     }
-
-//    ///////////////////////
-//    // Service Discovery //
-//    ///////////////////////
-//
-//    // TODO refactor these out into their own Class `UDPDiscovery` which extends UDP (+IConnection) or just IConnection
-//
-//    fun discoverConnections(context: Context) {
-//        val msg = "DISCOVER_RPIBOT"
-//        val socket = DatagramSocket(broadcast_port)
-//        socket.broadcast = true
-//        val packet = DatagramPacket(
-//            msg.toByteArray(),
-//            msg.length,
-//            getBroadcastAddress(context),
-//            serverBroadcastPort
-//        )
-//        socket.send(packet)
-//
-//        // TODO Listen for responses
-//    }
-//
-//    @Throws(IOException::class)
-//    fun getBroadcastAddress(context: Context): InetAddress? {
-//        val wifi: WifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-//        val dhcp = wifi.dhcpInfo
-//        val broadcast = dhcp.ipAddress and dhcp.netmask or dhcp.netmask.inv()
-//        val quads = ByteArray(4)
-//        for (k in 0..3) quads[k] = (broadcast shr k * 8 and 0xFF).toByte()
-//        return InetAddress.getByAddress(quads)
-//    }
 }
